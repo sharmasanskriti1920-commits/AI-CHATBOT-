8492479cc5037ec13c25d6a1c391d17970e3e33e
sharmasanskriti1920-commits <sharmasanskriti1920@gmail.com>
2026-08-09 17:18:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.post("/api/chat", async (req, res) => {
 
 // Frontend serve
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
