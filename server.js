479820b30b16604e858d82f0c1379ae42fc6bb86
sharmasanskriti1920-commits <sharmasanskriti1920@gmail.com>
2026-08-14 21:37:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,12 @@ app.get("/", (req, res) => {
 });
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-
+const model = genAI.getGenerativeModel({ 
+  model: "gemini-1.5-flash",
+  generationConfig: {
+    temperature: 0.9,
+  },
+});
 app.post("/api/chat", async (req, res) => {
   try {
     const { message } = req.body;
